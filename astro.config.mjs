fecd061e9c4718281736a91240d2ebbbd70189be
Jon-Hudak <shigshagor@gmail.com>
2023-04-29 20:50:41 -0400

    feat: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 import image from "@astrojs/image";
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  })],
-  site:"https://www.jonhudak.dev"
+  }), sitemap()],
+  site: "https://www.jonhudak.dev"
 });
