71ca6b679b84e6fdf5d59207272442d31e319959
Jon-Hudak <shigshagor@gmail.com>
2024-01-10 03:47:01 -0500

    chore: General cleanup (some style changes)

--- src/utils/cities.js
+++ src/utils/cities.js
@@ -1,2 +1,1 @@
-export const cities = ["Pittsburgh","Elizabeth","McKeesport","Carnegie"];
- 
+export const cities = ["Pittsburgh", "Elizabeth", "McKeesport", "Carnegie"];
--- src/utils/observer.js
+++ src/utils/observer.js
@@ -1,19 +1,22 @@
-export default function observer(elements, addClass, threshold=0.5, margin="0px 0px 0px 0px"){
-
-const observer = new IntersectionObserver(
+export default function observer(
+  elements,
+  addClass,
+  threshold = 0.5,
+  margin = "0px 0px 0px 0px",
+) {
+  const observer = new IntersectionObserver(
     (entries) => {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add(addClass);
-                observer.unobserve(entry.target);
-                console.log(entry.target, "intersect");
-            }
-        });
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add(addClass);
+          observer.unobserve(entry.target);
+          
+        }
+      });
     },
-    { threshold: threshold,
-    rootMargin: margin }
-    
-);
-    elements.forEach((item) => {
+    { threshold: threshold, rootMargin: margin },
+  );
+  elements.forEach((item) => {
     observer.observe(item);
-});}
+  });
+}
